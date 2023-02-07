de54f3894f2c5bc3b73b7f14c6780a7f844fc74f
OliverBrimdjam <oliverbrimdjam@hotmail.com>
2023-02-07 17:25:38 -0300

    refactor: main-template children render config; mock side-bar buttons;

--- src/app/components/templates/main-template/main-template.component.ts
+++ src/app/components/templates/main-template/main-template.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-main-template',
@@ -6,6 +6,12 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./main-template.component.scss']
 })
 export class MainTemplateComponent implements OnInit {
+  @Input() mainComponent: any;
+  buttonsArray: any[] = [
+    { nome: 'Home', color: 'primary', disabled: false },
+    { nome: 'Home', color: 'primary', disabled: false },
+    { nome: 'Home', color: 'primary', disabled: false },
+  ]
 
   constructor() { }
 
