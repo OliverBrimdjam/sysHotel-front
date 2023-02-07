418b5ef849cbbfd7808b2608dcb03b496a21c144
OliverBrimdjam <oliverbrimdjam@hotmail.com>
2023-02-07 10:59:17 -0300

    feature: button-icon component; main-logo component;

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,15 +4,23 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MainLogoComponent } from './components/atoms/main-logo/main-logo.component';
+import { ButtonIconComponent } from './components/atoms/button-icon/button-icon.component';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MainLogoComponent,
+    ButtonIconComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    MatButtonModule,
+    MatIconModule
   ],
   providers: [],
   bootstrap: [AppComponent]
