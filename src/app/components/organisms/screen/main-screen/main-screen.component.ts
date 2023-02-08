8fd9b8bcca1a02a15a175c6a6a21a1fc4853dbbf
OliverBrimdjam <oliverbrimdjam@hotmail.com>
2023-02-08 20:32:27 -0300

    refactor: main-screen layout update

--- src/app/components/organisms/screen/main-screen/main-screen.component.ts
+++ src/app/components/organisms/screen/main-screen/main-screen.component.ts
@@ -6,13 +6,19 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./main-screen.component.scss']
 })
 export class MainScreenComponent implements OnInit {
-  buttonsArray: any[] = [
+  roomStatusList: any[] = [
     { nome: 'Available', color: 'primary', disabled: false },
     { nome: 'Occupied', color: 'primary', disabled: false },
     { nome: 'Reserved', color: 'primary', disabled: false },
     { nome: 'Out of Order', color: 'primary', disabled: false },
     { nome: 'Maintenance', color: 'primary', disabled: false },
     { nome: 'cleaning', color: 'primary', disabled: false },
+  ]
+  reservesStatusList: any[] = [
+    { nome: 'Reserved', color: 'primary', disabled: false },
+    { nome: 'Checked In', color: 'primary', disabled: false },
+    { nome: 'Checked Out', color: 'primary', disabled: false },
+    { nome: 'Canceled', color: 'primary', disabled: false },
   ]
 
   constructor() { }
