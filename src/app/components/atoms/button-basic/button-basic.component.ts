3d1850385d679827de5c657e2cd4705627c5d410
OliverBrimdjam <oliverbrimdjam@hotmail.com>
2023-02-09 09:05:23 -0300

    feature: button-basic component implementation

---
+++ src/app/components/atoms/button-basic/button-basic.component.ts
@@ -0,0 +1,20 @@
+import { Component, OnInit, Input } from '@angular/core';
+
+@Component({
+  selector: 'app-button-basic',
+  templateUrl: './button-basic.component.html',
+  styleUrls: ['./button-basic.component.scss']
+})
+export class ButtonBasicComponent implements OnInit {
+  @Input() content: string = '';
+  @Input() colorSchema: 'primary' | 'secondary' | 'default' | '' = 'primary';
+  @Input() url: string | undefined = undefined;
+  @Input() action: () => void = () => { };
+  @Input() disabled: boolean = false;
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
