3cee5737cd314acf7699125a772b31ae60c8b8f1
OliverBrimdjam <oliverbrimdjam@hotmail.com>
2023-02-10 10:30:37 -0300

    feature: dialog-add-room component implement

--- src/app/components/molecules/dialog-add-room/dialog-add-room.component.ts
+++ src/app/components/molecules/dialog-add-room/dialog-add-room.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { MatDialogRef } from '@angular/material/dialog';
 
 @Component({
   selector: 'app-dialog-add-room',
@@ -7,9 +9,47 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DialogAddRoomComponent implements OnInit {
 
-  constructor() { }
+  addRoomForm!: FormGroup
+
+  constructor(
+    public dialogRef: MatDialogRef<DialogAddRoomComponent>
+  ) { }
 
   ngOnInit(): void {
+    this.addRoomForm = new FormGroup({
+      nameField: new FormControl('', [Validators.required, Validators.minLength(3)]),
+      priceField: new FormControl('', [Validators.required]),
+      statusField: new FormControl('', [Validators.required]),
+      typeField: new FormControl('', [Validators.required]),
+    });
+  }
+
+  get nameField() {
+    return this.addRoomForm.get('nameField')!;
+  }
+
+  get priceField() {
+    return this.addRoomForm.get('priceField')!;
+  }
+
+  get statusField() {
+    return this.addRoomForm.get('statusField')!;
+  }
+
+  get typeField() {
+    return this.addRoomForm.get('typeField')!;
+  }
+
+  submit() {
+    if (this.addRoomForm.invalid) {
+      return;
+    }
+    console.log('submit', this.addRoomForm.value);
+    this.dialogRef.close();
+  }
+
+  cancelSubmit(e: Event) {
+    e.preventDefault();
   }
 
 }
