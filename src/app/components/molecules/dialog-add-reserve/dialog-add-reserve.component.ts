18c3f3435e68d162ac47769712b77f342d694ab0
OliverBrimdjam <oliverbrimdjam@hotmail.com>
2023-02-10 10:30:10 -0300

    feature: dialog-add-reserve component implement

--- src/app/components/molecules/dialog-add-reserve/dialog-add-reserve.component.ts
+++ src/app/components/molecules/dialog-add-reserve/dialog-add-reserve.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { MatDialogRef } from '@angular/material/dialog';
 
 @Component({
   selector: 'app-dialog-add-reserve',
@@ -7,9 +9,57 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DialogAddReserveComponent implements OnInit {
 
-  constructor() { }
+  addRoomForm!: FormGroup
+
+  constructor(
+    public dialogRef: MatDialogRef<DialogAddReserveComponent>
+  ) { }
 
   ngOnInit(): void {
+    this.addRoomForm = new FormGroup({
+      roomField: new FormControl('', [Validators.required, Validators.minLength(3)]),
+      guestField: new FormControl('', [Validators.required]),
+      companionField: new FormControl('', [Validators.required]),
+      dateField: new FormControl('', [Validators.required]),
+      daysField: new FormControl('', [Validators.required]),
+      statusField: new FormControl('', [Validators.required]),
+    });
+  }
+
+  get roomField() {
+    return this.addRoomForm.get('roomField')!;
+  }
+
+  get guestField() {
+    return this.addRoomForm.get('guestField')!;
+  }
+
+  get companionField() {
+    return this.addRoomForm.get('companionField')!;
+  }
+
+  get dateField() {
+    return this.addRoomForm.get('dateField')!;
+  }
+
+  get daysField() {
+    return this.addRoomForm.get('daysField')!;
+  }
+
+  get statusField() {
+    return this.addRoomForm.get('statusField')!;
+  }
+
+  submit() {
+    if (this.addRoomForm.invalid) {
+      return;
+    }
+    console.log('submit', this.addRoomForm.value);
+    this.dialogRef.close();
+  }
+
+  cancelSubmit(e: Event) {
+    e.preventDefault();
   }
 
 }
