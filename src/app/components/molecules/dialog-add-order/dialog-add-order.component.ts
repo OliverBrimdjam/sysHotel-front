dc823159ac1506eaef2ae4ba9c68ad253f6ff74c
OliverBrimdjam <oliverbrimdjam@hotmail.com>
2023-02-10 01:18:00 -0300

    feature: dialog-add-order component implement

--- src/app/components/molecules/dialog-add-order/dialog-add-order.component.ts
+++ src/app/components/molecules/dialog-add-order/dialog-add-order.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { MatDialogRef } from '@angular/material/dialog';
 
 @Component({
   selector: 'app-dialog-add-order',
@@ -6,10 +8,36 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./dialog-add-order.component.scss']
 })
 export class DialogAddOrderComponent implements OnInit {
+  addGuestForm!: FormGroup
 
-  constructor() { }
+  constructor(
+    public dialogRef: MatDialogRef<DialogAddOrderComponent>
+  ) { }
 
   ngOnInit(): void {
+    this.addGuestForm = new FormGroup({
+      guestField: new FormControl('', [Validators.required, Validators.minLength(3)]),
+      serviceField: new FormControl('', [Validators.required]),
+    });
   }
 
+  get guestField() {
+    return this.addGuestForm.get('guestField')!;
+  }
+
+  get serviceField() {
+    return this.addGuestForm.get('serviceField')!;
+  }
+
+  submit() {
+    if (this.addGuestForm.invalid) {
+      return;
+    }
+    console.log('submit', this.addGuestForm.value);
+    this.dialogRef.close();
+  }
+
+  cancelSubmit(e: Event) {
+    e.preventDefault();
+  }
 }
