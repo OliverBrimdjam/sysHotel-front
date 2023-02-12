81fc2c962368c0b366ac17ecff2ce432ba3b1e89
OliverBrimdjam <oliverbrimdjam@hotmail.com>
2023-02-12 18:50:08 -0300

    feature: chart-overview component service implementation

--- src/app/components/molecules/table-chart-overview/table-chart-overview.component.ts
+++ src/app/components/molecules/table-chart-overview/table-chart-overview.component.ts
@@ -1,4 +1,9 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { FormControl } from '@angular/forms';
+import { MatPaginator } from '@angular/material/paginator';
+import { MatSort } from '@angular/material/sort';
+import { MatTableDataSource } from '@angular/material/table';
+import { RoomHistoryStateService } from 'src/app/store/room-history-state.service';
 
 @Component({
   selector: 'app-table-chart-overview',
@@ -6,10 +11,46 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./table-chart-overview.component.scss']
 })
 export class TableChartOverviewComponent implements OnInit {
+  collection: any[] | undefined = []
+  displayedColumns: string[] = ['id', 'lastChange', 'roomId', 'roomStatusId'];
+  dataSource!: MatTableDataSource<any>;
+  filterFormControl = new FormControl('');
 
-  constructor() { }
+
+  @ViewChild(MatPaginator)
+  paginator!: MatPaginator;
+  @ViewChild(MatSort)
+  sort: MatSort = new MatSort;
+
+
+  constructor(private roomHistoryStateService: RoomHistoryStateService) {}
 
   ngOnInit(): void {
+
+    this.getRoomHistoryList();
   }
 
+  ngAfterViewInit() {
+
+  }
+
+  applyFilter(event: Event) {
+    const filterValue = (event.target as HTMLInputElement).value;
+    this.dataSource.filter = filterValue.trim().toLowerCase();
+
+    if (this.dataSource.paginator) {
+      this.dataSource.paginator.firstPage();
+    }
+  }
+
+  getRoomHistoryList() {
+    this.roomHistoryStateService.getRoomHistoryState().subscribe(roomHistory => {
+      this.collection = roomHistory.map((roomEvent) => {
+        return { id: roomEvent.id, lastChange: roomEvent.lastChange, roomId: roomEvent.roomId, roomStatusId: roomEvent.roomStatusId }
+      });
+      this.dataSource = new MatTableDataSource(this.collection);
+      this.dataSource.paginator = this.paginator;
+      this.dataSource.sort = this.sort;
+    });
+  }
 }
