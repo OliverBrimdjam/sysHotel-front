6f5b9b63d88295e2e41261d98fd879da86d0032b
OliverBrimdjam <oliverbrimdjam@hotmail.com>
2023-02-11 21:46:15 -0300

    feature: table-room-overview component implement

---
+++ src/app/components/molecules/table-room-overview/table-room-overview.component.ts
@@ -0,0 +1,60 @@
+import { Component, OnInit,  ViewChild, Input } from '@angular/core';
+import { FormControl } from '@angular/forms';
+import { MatPaginator } from '@angular/material/paginator';
+import { MatSort } from '@angular/material/sort';
+import { MatTableDataSource } from '@angular/material/table';
+import { RoomService } from 'src/app/services/room.service';
+import { RoomStateService } from 'src/app/store/room-state.service';
+
+@Component({
+  selector: 'app-table-room-overview',
+  templateUrl: './table-room-overview.component.html',
+  styleUrls: ['./table-room-overview.component.scss']
+})
+export class TableRoomOverviewComponent implements OnInit {
+  // @Input() roomCollection: any[] | undefined = []
+  roomCollection: any[] | undefined = []
+  displayedColumns: string[] = ['Name', 'Status', 'Type'];
+  dataSource!: MatTableDataSource<any>;
+  filterFormControl = new FormControl('');
+
+
+  @ViewChild(MatPaginator)
+  paginator!: MatPaginator;
+  @ViewChild(MatSort)
+  sort: MatSort = new MatSort;
+
+
+  constructor(private roomState: RoomStateService) {}
+
+  ngOnInit(): void {
+
+    this.getRoomsList();
+    console.log('no onInit da table-room-overview', this.roomCollection);
+  }
+
+  ngAfterViewInit() {
+
+  }
+
+  applyFilter(event: Event) {
+    const filterValue = (event.target as HTMLInputElement).value;
+    this.dataSource.filter = filterValue.trim().toLowerCase();
+
+    if (this.dataSource.paginator) {
+      this.dataSource.paginator.firstPage();
+    }
+  }
+
+  getRoomsList() {
+    this.roomState.getRoomState().subscribe(rooms => {
+      this.roomCollection = rooms.map((room) => {
+        return { name: room.name, roomStatusId: room.roomStatusId, roomType: room.roomTypeId }
+      });
+      this.dataSource = new MatTableDataSource(this.roomCollection);
+      this.dataSource.paginator = this.paginator;
+      this.dataSource.sort = this.sort;
+    });
+  }
+
+}
