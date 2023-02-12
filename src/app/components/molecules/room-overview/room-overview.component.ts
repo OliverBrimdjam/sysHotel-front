0e472c141e78cee00b4efc32bdb265e210727af0
OliverBrimdjam <oliverbrimdjam@hotmail.com>
2023-02-11 21:55:13 -0300

    refactor: table-room-overview implemented

--- src/app/components/molecules/room-overview/room-overview.component.ts
+++ src/app/components/molecules/room-overview/room-overview.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
   selector: 'app-room-overview',
@@ -6,6 +6,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./room-overview.component.scss']
 })
 export class RoomOverviewComponent implements OnInit {
+  @Input() roomCollection = []
 
   constructor() { }
 
