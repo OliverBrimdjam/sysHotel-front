70871849e1aec11464e041bc7a80e468917d361c
OliverBrimdjam <oliverbrimdjam@hotmail.com>
2023-02-12 18:48:43 -0300

    chore: chart component minor changes

--- src/app/components/atoms/chart/chart.component.ts
+++ src/app/components/atoms/chart/chart.component.ts
@@ -6,39 +6,25 @@ import { ChartConfiguration, ChartOptions } from 'chart.js';
   templateUrl: './chart.component.html',
   styleUrls: ['./chart.component.scss']
 })
-export class ChartComponent implements OnInit {
+export class ChartComponent {
   title = 'ng2-charts-demo';
 
-  public lineChartData: ChartConfiguration<'line'>['data'] = {
-    labels: [
-      'January',
-      'February',
-      'March',
-      'April',
-      'May',
-      'June',
-      'July'
-    ],
+  public barChartLegend = true;
+  public barChartPlugins = [];
+
+  public barChartData: ChartConfiguration<'bar'>['data'] = {
+    labels: [ '2006', '2007', '2008', '2009', '2010', '2011', '2012' ],
     datasets: [
-      {
-        data: [ 65, 59, 80, 81, 56, 55, 40 ],
-        label: 'Series A',
-        fill: true,
-        tension: 0.5,
-        borderColor: 'black',
-        backgroundColor: 'rgba(255,0,0,0.3)'
-      }
+      { data: [ 65, 59, 80, 81, 56, 55, 40 ], label: 'Series A' },
+      { data: [ 28, 48, 40, 19, 86, 27, 90 ], label: 'Series B' }
     ]
   };
-  public lineChartOptions: ChartOptions<'line'> = {
-    responsive: false
+
+  public barChartOptions: ChartConfiguration<'bar'>['options'] = {
+    responsive: false,
   };
-  public lineChartLegend = true;
+
   constructor() {
-  }
-
-  ngOnInit(): void {
-
   }
 
 }
